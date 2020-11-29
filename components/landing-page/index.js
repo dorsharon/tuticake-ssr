473e79bf2516e39d4d2a8c8914d91304326b8b1a
Dor Sharon <dor@oriient.me>
2020-11-29 17:29:08 +0200

    Some adjustments to landing page

--- components/landing-page/index.js
+++ components/landing-page/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import {
     getBackgroundColor,
     getBreakpointAndDown,
     getBreakpointAndUp,
+    getCommonColor,
     getGradient,
 } from '../../utils/ThemeSelectors';
 import AnimatedLogoHeb from './AnimatedLogoHeb';
@@ -15,20 +16,30 @@ import Grid from '@material-ui/core/Grid';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { Container } from '@material-ui/core';
+
+const LandingPageWrapper = styled.div`
+    display: grid;
+    row-gap: 30px;
+
+    ${getBreakpointAndUp('lg')} {
+        grid-template-rows: 350px 1fr;
+    }
+
+    ${getBreakpointAndDown('sm')} {
+        grid-template-rows: 250px 1fr;
+    }
+
+    ${getBreakpointAndDown('xs')} {
+        grid-template-rows: 200px 1fr;
+    }
+`;
 
 const HeaderSection = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     background-image: ${getGradient()};
-
-    ${getBreakpointAndUp('sm')} {
-        min-height: 400px;
-    }
-
-    ${getBreakpointAndDown('xs')} {
-        min-height: 250px;
-    }
 `;
 
 const LogoCupcake1 = styled(LogoCupcakeSvg)`
@@ -93,92 +104,83 @@ const LogoCupcake2 = styled(LogoCupcakeSvg)`
     }
 `;
 
-const LandingPageWrapper = styled.div`
-    position: relative;
-    min-height: 600px;
-    width: 100%;
-`;
+const IntroductionWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-self: center;
 
-const SectionDivider = styled.div`
-    width: 100%;
-    overflow: hidden;
-
-    svg {
-        position: relative;
-        display: block;
-        width: calc(100% + 1.3px);
-        transform: rotateY(180deg);
-
-        ${getBreakpointAndUp('sm')} {
-            height: 180px;
-        }
-
-        ${getBreakpointAndDown('xs')} {
-            height: 100px;
-        }
-    }
-
-    path {
-        fill: ${getBackgroundColor()};
+    & > *:not(:last-child) {
+        margin-inline-end: 20px;
     }
 
     ${getBreakpointAndUp('lg')} {
-        bottom: 30%;
+        width: 70%;
     }
 
     ${getBreakpointAndDown('md')} {
-        bottom: 45%;
+        width: 85%;
+        flex-direction: column;
     }
 
     ${getBreakpointAndDown('sm')} {
-        bottom: 55%;
+        width: 90%;
+        flex-direction: column;
     }
 `;
 
-const ProfileAndIntroductionWrapper = styled.div`
-    position: absolute;
+const Waves = styled.svg`
+    position: relative;
+    width: 100%;
+    height: 15vh;
+    margin-bottom: -7px; /*Fix for safari gap*/
+    min-height: 100px;
+    max-height: 150px;
 
-    ${getBreakpointAndUp('sm')} {
-        top: 200px;
-    }
-
-    ${getBreakpointAndDown('xs')} {
-        top: 140px;
-    }
-`;
-
-const ProfileImageCard = styled(Card)``;
-
-const ProfileImageCardContent = styled(CardContent)`
-    && {
-        padding: 10px;
+    ${getBreakpointAndDown('sm')} {
+        height: 50px;
+        min-height: 50px;
     }
 `;
 
-const ProfileImage = styled.img.attrs(() => ({
-    alt: 'profile',
-}))`
-    ${getBreakpointAndUp('sm')} {
-        height: 250px;
+const ParallaxAnimation = keyframes`
+    0% {
+        transform: translate3d(-90px, 0, 0);
     }
-
-    ${getBreakpointAndDown('xs')} {
-        height: 200px;
+    100% {
+        transform: translate3d(85px, 0, 0);
     }
 `;
 
-const ProfileImageSkeleton = styled(Skeleton).attrs(() => ({
-    variant: 'rect',
-}))`
-    ${getBreakpointAndUp('sm')} {
-        height: 250px;
-        width: 180px;
+const Parallax = styled.g`
+    & > use {
+        animation: ${ParallaxAnimation} 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
     }
 
-    ${getBreakpointAndDown('xs')} {
-        height: 200px;
-        width: 145px;
+    & > use:nth-child(1) {
+        animation-delay: -2s;
+        animation-duration: 7s;
     }
+
+    & > use:nth-child(2) {
+        animation-delay: -3s;
+        animation-duration: 10s;
+    }
+
+    & > use:nth-child(3) {
+        animation-delay: -4s;
+        animation-duration: 13s;
+    }
+
+    & > use:nth-child(4) {
+        animation-delay: -5s;
+        animation-duration: 20s;
+    }
+`;
+
+const ProfileImage = styled(Image)`
+    border: 10px solid ${getCommonColor('white')};
+    filter: drop-shadow(2px 4px 6px black);
 `;
 
 export default function LandingPage() {
@@ -194,45 +196,33 @@ export default function LandingPage() {
                 <LogoCupcake1 />
                 <LogoCupcake2 />
 
-                <SectionDivider>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 1200 120"
-                        preserveAspectRatio="none"
-                    >
-                        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" />
-                    </svg>
-                </SectionDivider>
+                <Waves viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+                    <defs>
+                        <path
+                            id="gentle-wave"
+                            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                        />
+                    </defs>
+
+                    <Parallax>
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </Parallax>
+                </Waves>
             </HeaderSection>
 
-            <Grid
-                container
-                direction={'column'}
-                alignItems={'center'}
-                component={ProfileAndIntroductionWrapper}
-            >
-                <Grid item>
-                    <ProfileImageCard>
-                        <ProfileImageCardContent>
-                            <Image
-                                alt={'profile'}
-                                src={'/profile.jpeg'}
-                                height={220.5}
-                                width={160}
-                            />
-                        </ProfileImageCardContent>
-                    </ProfileImageCard>
-                </Grid>
+            <IntroductionWrapper>
+                <ProfileImage alt={'profile'} src={'/profile.jpeg'} height={220.5} width={160} />
 
-                <Grid item xs={9}>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    </p>
-                </Grid>
-            </Grid>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse
+                </p>
+            </IntroductionWrapper>
         </LandingPageWrapper>
     );
 }
