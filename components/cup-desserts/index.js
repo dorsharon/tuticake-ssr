faad8dc078e19aba60e38713cdaf0ddba9973ba2
Dor Sharon <dor@oriient.me>
2021-01-15 14:11:39 +0200

    Adjust cup desserts images

--- components/cup-desserts/index.js
+++ components/cup-desserts/index.js
@@ -1,20 +1,18 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Grid, Typography, useMediaQuery } from '@material-ui/core';
-import { useQuery, useQueryCache } from 'react-query';
+import { useQueryCache } from 'react-query';
 import {
-    getBackgroundColor,
     getBreakpointAndDown,
     getBreakpointAndUp,
     getCommonColor,
     getGradient,
     getPrimaryColor,
 } from '../../utils/ThemeSelectors';
-import { Skeleton } from '@material-ui/lab';
 import { Button } from '@material-ui/core';
 import CupDessertsOrderForm from './CupDessertsOrderForm';
 import { useI18n } from 'next-localization';
-import { CUP_DESSERTS_EXAMPLE_IMAGES, PRODUCTS } from '../../constants/queryKeys';
+import { PRODUCTS } from '../../constants/queryKeys';
 import { CUP_DESSERT, CUP_DESSERTS_BOX_SET } from '../../constants/productTypes';
 import Image from 'next/image';
 import Carousel from '../common/ImageCarousel';
@@ -99,7 +97,6 @@ const ExampleImagesWrapper = styled.div`
         grid-template-rows: repeat(2, 250px);
 
         & > div {
-            filter: drop-shadow(2px 4px 6px black);
             transform: rotate(20deg);
 
             &:nth-child(1) {
@@ -128,14 +125,22 @@ const ExampleImagesWrapper = styled.div`
     }
 `;
 
+const exampleImages = [1, 2, 3, 4].map((i) => (
+    <ImageWrapper key={i}>
+        <Image
+            alt={`example-image-${i}`}
+            src={`website-assets/cup-desserts-examples/example${i}.webp`}
+            width={300}
+            height={300}
+            objectFit={'cover'}
+        />
+    </ImageWrapper>
+));
+
 export default function CupDesserts() {
     const { t } = useI18n();
 
     const [isOrderFormOpen, setIsOrderFormOpen] = useState(false);
-
-    const exampleImageUrls = [1, 2, 3, 4].map(
-        (i) => `website-assets/cup-desserts-examples/example${i}.webp`,
-    );
 
     const cache = useQueryCache();
 
@@ -155,35 +160,7 @@ export default function CupDesserts() {
     return (
         <CupDessertsWrapper>
             <ExampleImagesWrapper>
-                {isInBreakpointSm ? (
-                    <ImageCarousel>
-                        {exampleImageUrls?.map((url, index) => (
-                            <ImageWrapper key={url}>
-                                <Image
-                                    alt={`example-image-${index}`}
-                                    src={url}
-                                    width={300}
-                                    height={300}
-                                    // layout={'fill'}
-                                    objectFit={'cover'}
-                                />
-                            </ImageWrapper>
-                        ))}
-                    </ImageCarousel>
-                ) : (
-                    exampleImageUrls?.map((url, index) => (
-                        <ImageWrapper key={url}>
-                            <Image
-                                alt={`example-image-${index}`}
-                                src={url}
-                                width={300}
-                                height={300}
-                                // layout={'fill'}
-                                objectFit={'cover'}
-                            />
-                        </ImageWrapper>
-                    ))
-                )}
+                {isInBreakpointSm ? <ImageCarousel>{exampleImages}</ImageCarousel> : exampleImages}
             </ExampleImagesWrapper>
 
             {isOrderFormOpen ? (
